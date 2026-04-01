3b973d7dc9348ddd657b6b0b710a5d30f40a818d
Vyctoria Karina <vyctoriak@gmail.com>
2026-03-31 21:33:20 -0300

    feat: integrate PrismaPg adapter with pg driver for database connection management

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy {
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(configService: ConfigService) {
+    const connectionString = configService.getOrThrow<string>('DATABASE_URL');
+    const adapter = new PrismaPg(connectionString);
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
